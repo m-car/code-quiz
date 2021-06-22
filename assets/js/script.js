729b0d1680718710ec3cd971794870c07551b847
Marko Caric <pbmc117@gmail.com>
2021-06-21 20:42:24 -0700

    Initial HTML, CSS, JS added and linked.

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+var doc = window.document;
+console.log(window);
+console.log(doc);
+var startEl = document.getElementById('#start-button');
+var resetEl = document.getElementById('#reset-button');
+var scoreBrd = document.getElementById('.scoreboard');
+console.log(startEl);
