bd52a42001b03a799a5d5fcc565e91cb49675305
Marko Caric <pbmc117@gmail.com>
2021-06-22 18:53:02 -0700

    added more relevant questions/answers, question and answer displays on page when clicking start, timer does not work

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,44 +4,6 @@ console.log(window);
 console.log('Document Body: ');
 console.log(document.body);
 
-var questions = ['why is the ocean blue', 'what color is the truck?', 'do you like coffee?'];
-var answers = ['the sky', 'red', 'yes']
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//Create elements
-// var h1El = document.createElement('h1');
-// var questionEl = document.createElement('div');
-// var answersEl = document.createElement('div');
-// var startBtnEl = document.createElement('button');
-// var resetBtnEl = document.createElement('button');
-// var scoreBoard = document.createElement('div');
-// var header = document.getElementById('#header');
-
-//text content elements
-// h1El.textcontent = "Welcome to Code Quiz";
-// document.body.appendChild(h1El);
-// console.log(h1El.textContent);
-
-
-
-
-
 var divTags = document.querySelectorAll('div');
 
 var gameContainer = document.getElementById('game-container');
@@ -52,22 +14,96 @@ var resetEl = document.getElementById('reset-button');
 var scoreBrd = document.getElementById('scoreboard');
 var timerEl = document.getElementById('timer');
 
+
+
+
+
+//QUESTION/ANSWER OBJECTS. These will be questions with multiple answer values
+var question1 = {
+    q: "Inside which HTML element do we put the JavaScript?",
+    a1: '<javascript>',
+    a2: '<scripting>',
+    a3: '<script>',
+    a4: '<js>',
+    correct: 'a3',
+
+}
+
+var question2 = {
+    q: "What is the correct JavaScript syntax to change the content of the HTML element below? \n<p id=\"demo\">This is a demonstration.</p>",
+    a1: 'document.getElementById("demo").innerHTML = "Hello World!"',
+    a2: 'document.getElementById("p").intterHTML ="Hello World!"',
+    a3: '#demo.innerHTML = "Hello World!"',
+    a4: 'document.getElement("p").innerHTML = "Hello World!"',
+    correct:'a4',
+}
+
+var question3 = {
+    q: "Where is the correct place to insert a JavaScript?",
+    a1: 'Both the <head> section and the <body> section are correct.',
+    a2: 'The <head> section.',
+    a3: 'The <body> section.',
+    a4: 'The <footer> section.',
+    correct: 'a3',
+}
+var question4 = {
+    q: "What is the correct syntax for referring to an external script called 'xxx.js'?",
+    a1: '<script name = "xxx.js">',
+    a2: '<script href = "xxs.js">',
+    a3: '<script src = "xxs.js">',
+    a4: '<script alt = "xxs.js">',
+    correct: 'a3',
+}
+var question5 = {
+    q: "The external JavaScript file must contain the <script> tag?",
+    a1: 'True',
+    a2: 'False',
+    a3: 'Both',
+    a4: 'Dont choose this one',
+    correct: 'a2',
+}
+var question6 = {
+    q: "How do you write 'Hello World' in an alert box?",
+    a1: 'alert("Hello world");',
+    a2: 'alertBox("Hello World");',
+    a3: 'msgBox("Hello World");',
+    a4: 'msg("Hello World");',
+    correct: 'a1',
+}
+var question7 = {
+    q: "How do you create a function in JavaScript?",
+    a1: 'function = myFunction()',
+    a2: 'function:myFunction()',
+    a3: 'function myFunction()',
+    a4: 'take me to funcy town',
+    correct: 'a1',
+}
+
+//Question Bank Declaration
+var questionBank = [question1, question2, question3, question4, question5, question6, question7]
+console.log(questionBank)
+
 //initial CSS 
 header.children[0].style.fontSize = '40px';
 
 divTags[0].setAttribute('style','font-size: 50px');
 divTags[1].setAttribute('style','font-size: 20px; font-weight: bold; ');
 
+
+
+
 // display landing screen: welcome message, scoreboard 0, timer 
 //changing content
 questionBox.textContent = 'questions will go here. Push Start!';
 answerbox.textContent = 'answers will go here';
+scoreBrd.textContent = 'wins: 0';
 
 //***FUNCTIONS***
 
 //Timer
-var secondsLeft = 15;
+var secondsLeft;
 function timer(){
+    secondsLeft = 15;
     var timerInterval = setInterval(function(){
         if (secondsLeft < 0){
             clearInterval(timerInterval);
@@ -89,15 +125,28 @@ function rand(max){
 
 // when I click START ,
 startEl.addEventListener('click', function(){
+    //reset text on page
+    answerbox.textContent='';
     //start timer
     timer();
-    i = rand(questions.length);
+    var i = rand(questionBank.length);
     //questions selected from a pool of questions RANDOMLY
-    questionBox.textContent= questions[i];
-    //answers displayed from pool of answers per question. array, key value pairs?
-    var listEl = document.createElement('li');
-    listEl.setAttribute('value','answer test');
-    answerbox.children[0].appendChild(listEl);
+    questionBox.textContent= questionBank[i].q;
+    //answers displayed from pool of answers per question. array, key value pairs, objects?
+    var listEl1 = document.createElement('li');
+    listEl1.textContent = questionBank[i].a1;
+    answerbox.appendChild(listEl1);
+    var listEl2 = document.createElement('li');
+    listEl2.textContent = questionBank[i].a2;
+    answerbox.appendChild(listEl2);
+    var listEl3 = document.createElement('li');
+    listEl3.textContent = questionBank[i].a3;
+    answerbox.appendChild(listEl3);
+    var listEl4 = document.createElement('li');
+    listEl4.textContent = questionBank[i].a4;
+    answerbox.appendChild(listEl4);
+   
+
     
 })
 
